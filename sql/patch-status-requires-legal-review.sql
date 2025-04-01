a071ece267aaabbe9313849ef3b3bd63783ccc10
drorsnir <FreedomFighterSparrow@gmail.com>
2025-04-01 08:42:35 +0300

    Bugfix for tms_requires_legal_review - should use 'not-required'
    ...with a hyphen, not an underscore.

--- sql/patch-status-requires-legal-review.sql
+++ sql/patch-status-requires-legal-review.sql
@@ -1,3 +1,3 @@
 -- Add the requires_legal_review field to tms_status table
 ALTER TABLE /*_*/tm_status
-	ADD COLUMN tms_requires_legal_review ENUM('not_required', 'required', 'completed') DEFAULT 'not_required';
+	ADD COLUMN tms_requires_legal_review ENUM('not-required', 'required', 'completed') DEFAULT 'not-required';
